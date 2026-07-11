b5669b4c5b9ea55e4967c27c0b692911d4cb8924
NUKASANI VENKATA VINOD <vinodnukasani58@gmail.com>
2026-07-11 22:49:54 +0530

    Simple_Calculator

---
+++ Simple_Calculator.py
@@ -0,0 +1,18 @@
+n1=int(input("Enter First number: "))
+op=input("Enter operator(+,-,*,**,%,/,//): ")
+n2=int(input("Enter Second number: "))
+if op=="+":
+    print("Answer :",n1+n2)
+elif op=="-":
+    print("Answer :",n1-n2)
+elif op=="*":
+    print("Answer :",n1*n2)
+elif op=="**":
+    print("Answer :",n1**n2)
+elif op=="%":
+    print("Answer :",n1%n2)
+elif op=="/":
+    print("Answer :",n1/n2)
+elif op=="//":
+    print("Answer :",n1//n2)
+    
